df89efb85a23a7ce9d07084e4df9752b67aae033
Sebastian <bastianonofrei@gmail.com>
2023-06-25 01:39:43 +0300

    Implemented Our products - with mock data . Future will have a CMS where i get the product data from ;)

--- src/components/molecules/product/Product.jsx
+++ src/components/molecules/product/Product.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Product.scss';
-
+import Product_img from '../../atoms/product_img/Product_img';
+import Button from '../../atoms/button/Button';
 const Product = (props) => {
+
   return (
     <div className='product'>
-        
+         <Product_img source={props.product.imageURL} product_name={props.product.product_name}/>
+         <h5>{props.product.product_name}</h5>
+         <p className='price'>LEI {props.product.price}</p>
+         <Button text='Buy Now' />
     </div>
   )
 }
--- src/components/organisms/our_products/Our_products.jsx
+++ src/components/organisms/our_products/Our_products.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import './Our_products.scss';
+import banana_strawberry from "../../../assets/bananastrawberry.png";
+import chocolate from "../../../assets/chocolate.png";
+import peach from "../../../assets/peach.png";
+import Product from '../../molecules/product/Product';
 
 const Our_products = () => {
-  return (
-		<>
+/********************
+ *    MOCK DATA     *
+ ********************/
+
+/*********************************************************************************
+ * I WILL IMPLEMENT THE PRODUCTS WITH A CMS - PRISMIC AFTER FINISHING THE DESIGN *
+ *********************************************************************************/
+
+const products = [
+{
+    imageURL: banana_strawberry,
+    product_name: "Banana strawberry",
+    price: 4.99,
+},
+{
+    imageURL: chocolate,
+    product_name: "Yogurt chocolate",
+    price: 2.99,
+},
+{
+    imageURL: peach,
+    product_name: "Yogurt peach",
+    price: 3.99,
+},
+];
+
+	return (
+		<main>
 			<h3>Our Products</h3>
 			<section className="our_products">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-					quaerat ab assumenda, sit et ratione excepturi facere nulla commodi.
-					Eos.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-					quaerat ab assumenda, sit et ratione excepturi facere nulla commodi.
-					Eos.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-					quaerat ab assumenda, sit et ratione excepturi facere nulla commodi.
-					Eos.
-				</p>
-			</section>
-		</>
+                {products.map(product=>{
+            return(
+                <Product product={product}/>
+            )
+         })}
+            </section>
+		</main>
 	);
 }
 
